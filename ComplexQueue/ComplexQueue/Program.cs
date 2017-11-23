cde3de1393fe32ce9a2d2591417f4ff537472580
$ ! L \\// A <fsatka@mail.ru>
2017-11-23 18:00:46 +0300

    Add files via upload

---
+++ ComplexQueue/ComplexQueue/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using ClassComplex;
+
+namespace ComplexQueue
+{
+    class Program
+    {
+        static void Main()
+        {
+            Complex z1 = new Complex(1, 1);
+            Complex z2 = new Complex(3, 5);
+            Complex z3 = new Complex(2, 3);
+                    
+            Queue L1 = new Queue();
+            L1.Engueue(z1);
+            L1.Engueue(z2);
+            L1.Engueue(z3);
+            L1.Engueue(z1);
+
+            Console.WriteLine("Вывод с ToString в последнем элементе");
+            L1.Print1();
+            Console.WriteLine("Вывод без ToString в последнем элементе");
+            L1.Print2();
+
+            Console.WriteLine("----------------------------------------------------------");
+            Console.WriteLine("\nN(L) = {0}",L1.Count());
+            Console.WriteLine("L1.Peek() = {0}", L1.Peek());
+            Console.WriteLine("----------------------------------------------------------");
+
+            Console.WriteLine("L1.Dequeue()");
+            L1.Dequeue();
+            L1.Print1();
+            Console.WriteLine("\nN(L) = {0}", L1.Count());
+
+            Console.WriteLine("----------------------------------------------------------");
+            z2.Re = 0;
+            L1.Print1();
+            Console.ReadLine();
+           
+        }
+    }
+}
---
+++ ComplexQueue/ComplexQueue/Queue.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using ClassComplex;
+
+namespace ComplexQueue
+{
+    class Queue
+    {
+        private Node _first, _last;
+
+        #region Добавляем элемент в очередь
+        public void Engueue(Complex z)
+        {
+            Node temp = new Node(z);
+            if(_last == null)
+            {
+                _last = temp;
+                _first = temp;
+            }
+            else
+            {
+                _last.Next = temp;
+                _last = _last.Next;
+            }
+        }
+        #endregion Убираем элемент
+
+        #region Убираем элемент
+        public Complex Dequeue()
+        {
+            if (_first == null)
+                return null;
+            else
+            {
+                Complex z = _first.Data;
+                _first = _first.Next;
+                if (_first == null) _last = null;
+                return z;
+            }
+        }
+        #endregion
+
+        #region Просто возвращаем 1ый элемент
+        public Complex Peek()
+        {
+            return _first.Data;
+        }
+        #endregion
+
+        #region Счётчик
+        public int Count()
+        {
+            int i = 0;
+            Node buf = _first;
+            while(buf != null)
+            {
+                buf = buf.Next;
+                i++;
+            }
+            return i;
+        }
+        #endregion
+
+        #region Вывод
+        public void Print1()
+        {
+            if (_first == null)
+            {
+                Console.WriteLine("Queue is empty");
+                return;
+            }
+
+            Node buf = _first;
+            while (buf != null)
+            {
+                if (buf.Next != null)
+                    Console.Write("{0} --> ", buf.Data);
+                else
+                    Console.WriteLine(buf.Data.ToString());
+                buf = buf.Next;
+            }
+        }
+
+        public void Print2()
+        {
+            if (_first == null)
+            {
+                Console.WriteLine("Queue is empty");
+                return;
+            }
+
+            Node buf = _first;
+            while (buf != null)
+            {
+                if (buf.Next != null)
+                    Console.Write("{0} --> ", buf.Data);
+                else
+                    Console.WriteLine(buf.Data);
+                buf = buf.Next;
+            }
+        }
+        #endregion
+
+        #region элементы очереди
+        private class Node
+        {
+            public Node(Complex data, Node next = null)
+            {
+                Data = data;
+                Next = next;
+            }
+
+            public Complex Data { get; set;}
+            public Node Next { get; set;}
+        }
+        #endregion
+
+    }
+}
