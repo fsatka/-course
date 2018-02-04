4a4ec621d602321e70748191933c35c6a2c8cc92
Roman <fsatka@mail.ru>
2018-02-04 11:22:58 +0600

    This programm can save information about Employee in xml format

---
+++ TableOfPersonal/TableOfPersonal/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+using System.IO;
+
+namespace TableOfPersonal
+{
+    class Program
+    {
+        static void Main()
+        {
+            string name = "";
+            string surname = "";
+            string patranymic = "";
+            string phone = "";
+            string address = "";
+
+            //Как использовать программу
+            Information();
+
+            List<Employee> mens = new List<Employee>();
+
+
+
+            if (File.Exists(Environment.CurrentDirectory + "\\Data.xml"))
+                using (FileStream stream = new FileStream("Data.xml", FileMode.Open))
+                {
+                    XmlSerializer serializer = new XmlSerializer(typeof(List<Employee>));
+                    mens = (List<Employee>)serializer.Deserialize(stream);
+                }
+            else
+            {
+                Console.WriteLine("_________________________");
+                Console.WriteLine("| Сохранения не найдены |");
+            }
+            
+
+            string exp = Console.ReadLine();
+            while(!exp.Equals("quit"))
+            {
+                if(exp.Equals("Add"))
+                {
+                    Employee men = new Employee();
+                    Console.Write("name: ");
+                    men.name = Console.ReadLine();
+                    Console.Write("surname: ");
+                    men.surname = Console.ReadLine();
+                    Console.Write("patarnymic: ");
+                    men.patronymic = Console.ReadLine();
+                    Console.Write("phone: ");
+                    men.phone = Console.ReadLine();
+                    Console.Write("address: ");
+                    men.address = Console.ReadLine();
+                    Console.WriteLine("--------------------------------------");
+                    mens.Add(men);
+                }
+
+                if (exp.Equals("Search"))
+                {
+                    Console.Write("name: ");
+                    name = Console.ReadLine();
+                    Console.Write("surname: ");
+                    surname = Console.ReadLine();
+                    Console.Write("patranymic: ");
+                    patranymic = Console.ReadLine();
+                    Console.Write("phone: ");
+                    phone = Console.ReadLine();
+                    Console.Write("address: ");
+                    address = Console.ReadLine();
+                    Console.WriteLine("--------------------------------------");
+
+                    Employee.Print(Employee.TableSearch(mens, name, surname, patranymic, phone, address));
+                    Console.WriteLine("--------------------------------------");
+                }
+
+                exp = Console.ReadLine();
+               
+            }
+
+            using (FileStream stream = new FileStream("Data.xml", FileMode.OpenOrCreate))
+            {
+                XmlSerializer serializer = new XmlSerializer(typeof(List<Employee>));
+                serializer.Serialize(stream, mens);
+            }
+        }
+
+        static void Information()
+        {
+            Console.WriteLine("Список команд:\n 1) Add - добавить информацию в таблицу\n 2) Search - найти информацию из таблицы по заданным данным\n 3) quit - выход из программы\n");
+            if (!File.Exists(Environment.CurrentDirectory + "\\Data.xml"))
+                Console.WriteLine("Примечание: если отсутствует элемент информации при поиске или добавлении, просто нажмите Enter\n" +
+                "Примечание: Ввод ФИО задаётся по шаблону(сначал идут одна или более заглавных букв алфавита лат или кирилица," +
+                " затем строчные, допускается пробел) в противном случае строка останется пустой\n" +
+                "Примечание: Ввод номера допусается в формате 8**********. О вводе номеров подробнее https://habrahabr.ru/post/110731/");
+        }
+    }
+}
---
+++ TableOfPersonal/TableOfPersonal/Table.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace TableOfPersonal
+{
+        [XmlRoot]
+        public class Employee
+        {
+            //поля сотрудника (по ним инициализируем xml)
+            [XmlAnyElement]
+            private string _name;
+            [XmlAnyElement]
+            private string _surname;
+            [XmlAnyElement]
+            private string _patronymic;
+            [XmlAnyElement]
+            private string _phone;
+            [XmlAnyElement]
+            private string _address;
+
+            //для парсинга полей класса
+            private static Regex pattern1 = new Regex(@"(^[A-Z]+[a-z-]*?|[А-ЯЁ]+[а-яё-]*?)$");
+            private static Regex pattern2 = new Regex(@"^((8|\+7)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}$"); // взято с https://habrahabr.ru/post/110731/
+        #region Свойства  
+        public Employee()
+            {
+                _name = "";
+                _surname = "";
+                _patronymic = "";
+                _phone = "";
+                _address = "";
+            }
+
+            public string name
+            {
+                get { return _name; }
+                set
+                {
+                    if (pattern1.IsMatch(value))
+                        _name = value;
+                    else
+                        value = "";
+                }
+
+            }
+
+            public string surname
+            {
+                get { return _surname; }
+                set
+                {
+                if (pattern1.IsMatch(value))
+                    _surname = value;
+                else
+                    value = "";
+                    
+                }
+            }
+
+            public string patronymic
+            {
+                get { return _patronymic; }
+                set
+                {
+                    if (pattern1.IsMatch(value))
+                        _patronymic = value;
+                    else
+                        value = "";
+                }
+            }
+
+            public string phone
+            {
+                get { return _phone; }
+                set
+                {
+                    if (pattern2.IsMatch(value))
+                        _phone = value;
+                    else
+                        value = "";
+                }
+            }
+
+        public string address
+        {
+            get { return _address; }
+            set
+            { 
+                    _address = value;
+            }
+        }
+        #endregion
+
+
+        //поиск информации по полям объекта
+        public static bool Search(Employee men, string name = "", string surname = "", string patronymic = "",  string phone = "", string address = "")
+        {
+            int i = 0, j = 0;
+            if (!name.Equals(""))
+            {
+                j++;
+                if (men.name.Equals(name))
+                    i++;
+            }
+            if (!surname.Equals(""))
+            {
+                j++;
+                if (men.surname.Equals(surname))
+                    i++;
+            }
+            if (!patronymic.Equals(""))
+            {
+                j++;
+                if (men.patronymic.Equals(patronymic))
+                    i++;
+            }
+            if (!phone.Equals(""))
+            {
+                j++;
+                if (men.phone.Equals(phone))
+                    i++;
+            }
+            if (!address.Equals(""))
+            {
+                j++;
+                if (men.address.Equals(address))
+                    i++;
+            }
+
+            if (i == j)
+                return true;
+            else
+                return false;
+        }
+
+        //поиск в листе
+        public static List<Employee> TableSearch(List<Employee> mens, string name = "", string surname = "", string patronymic = "", string phone = "", string address = "")
+        {
+            List<Employee> Out = new List<Employee>();
+            foreach (Employee men in mens)
+                if (Search(men, name, surname, patronymic, phone, address))
+                    Out.Add(men);
+            return Out;
+        }
+
+        //Вывод
+        static public void Print(List<Employee> mens)
+        {
+            if(mens.Count != 0)
+            {
+                foreach(Employee men in mens)
+                    Console.WriteLine("name: {0};         surname: {1};            patrnymic: {2};            phone: {3};            address: {4}", men.name, men.surname, men.patronymic, men.phone, men.address);
+            }
+            else Console.WriteLine("Пусто");
+        }
+    }
+    
+}
